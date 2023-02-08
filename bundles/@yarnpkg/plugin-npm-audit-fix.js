544f7d02ae6c94d59e09af0b150e6ee81b05bfca
Sargun Vohra <sargun.vohra@gmail.com>
2023-02-08 00:12:28 -0800

    proof of concept

--- bundles/@yarnpkg/plugin-npm-audit-fix.js
+++ bundles/@yarnpkg/plugin-npm-audit-fix.js
@@ -40,20 +40,167 @@ var plugin = (() => {
   __export(sources_exports, {
     default: () => sources_default
   });
+  var import_stream = __require("stream");
   var import_cli = __require("@yarnpkg/cli");
-  var import_clipanion = __require("clipanion");
+  var import_core = __require("@yarnpkg/core");
   var t = __toESM(__require("typanion"));
-  var STRATEGIES = [`TODO`];
+  var isAuditResult = t.isObject(
+    {
+      advisories: t.isDict(
+        t.isObject(
+          {
+            module_name: t.isString(),
+            vulnerable_versions: t.isString(),
+            patched_versions: t.isString()
+          },
+          { extra: t.isDict(t.isUnknown()) }
+        )
+      )
+    },
+    { extra: t.isDict(t.isUnknown()) }
+  );
   var NpmAuditFixCommand = class extends import_cli.BaseCommand {
-    constructor() {
-      super(...arguments);
-      this.strategy = import_clipanion.Option.String(`--strategy`, `TODO`, {
-        description: `Set which strategy to attempt to fix advisories`,
-        validator: t.isEnum(STRATEGIES)
-      });
+    async execute() {
+      const state = await this.initState();
+      const { configuration, project, cache } = state;
+      const report = await import_core.StreamReport.start(
+        {
+          configuration,
+          stdout: this.context.stdout
+        },
+        async (report2) => {
+          const advisories = await report2.startTimerPromise(
+            `Audit step`,
+            () => this.getAdvisories()
+          );
+          for (const advisory of advisories) {
+            const fmtName = import_core.structUtils.prettyIdent(
+              configuration,
+              import_core.structUtils.parseIdent(advisory.module_name)
+            );
+            const fmtVulnRange = import_core.structUtils.prettyRange(
+              configuration,
+              advisory.vulnerable_versions
+            );
+            const fmtPatchRange = import_core.structUtils.prettyRange(
+              configuration,
+              advisory.patched_versions
+            );
+            await report2.startTimerPromise(
+              `Advisory for ${fmtName} at ${fmtVulnRange}, patched at ${fmtPatchRange}`,
+              () => this.handleAdvisory(report2, state, advisory)
+            );
+          }
+          await project.install({
+            report: report2,
+            cache,
+            mode: import_core.InstallMode.UpdateLockfile
+          });
+        }
+      );
+      return report.exitCode();
     }
-    execute() {
-      return Promise.resolve(1);
+    async handleAdvisory(report, state, advisory) {
+      const { configuration, resolver, project, cache } = state;
+      const ident = import_core.structUtils.parseIdent(advisory.module_name);
+      for (const descriptor of project.storedDescriptors.values()) {
+        if (!import_core.structUtils.areIdentsEqual(descriptor, ident)) {
+          continue;
+        }
+        const locator = project.storedPackages.get(
+          project.storedResolutions.get(descriptor.descriptorHash)
+        );
+        if (!import_core.semverUtils.satisfiesWithPrereleases(
+          locator.version,
+          advisory.vulnerable_versions
+        )) {
+          continue;
+        }
+        report.reportInfo(
+          import_core.MessageName.UNNAMED,
+          `Found vulnerable ${import_core.structUtils.prettyLocator(
+            configuration,
+            locator
+          )} (via ${import_core.structUtils.prettyRange(configuration, descriptor.range)})`
+        );
+        const candidates = await resolver.getCandidates(descriptor, /* @__PURE__ */ new Map(), {
+          project,
+          report,
+          resolver
+        });
+        const preferred = candidates[0];
+        if (!preferred) {
+          report.reportError(
+            import_core.MessageName.UNNAMED,
+            `No candidates found for ${import_core.structUtils.prettyDescriptor(
+              configuration,
+              descriptor
+            )}`
+          );
+          continue;
+        }
+        const pkg = await resolver.resolve(preferred, {
+          project,
+          report,
+          resolver
+        });
+        if (!import_core.semverUtils.satisfiesWithPrereleases(
+          pkg.version,
+          advisory.patched_versions
+        )) {
+          report.reportWarning(
+            import_core.MessageName.UNNAMED,
+            `No compatible patched version found for ${import_core.structUtils.prettyDescriptor(
+              configuration,
+              descriptor
+            )}`
+          );
+          continue;
+        }
+        report.reportInfo(
+          import_core.MessageName.UNNAMED,
+          `Attempting to upgrade ${import_core.structUtils.prettyDescriptor(
+            configuration,
+            descriptor
+          )} to ${import_core.structUtils.prettyLocator(configuration, pkg)}`
+        );
+        project.storedResolutions.delete(descriptor.descriptorHash);
+        project.storedPackages.delete(locator.locatorHash);
+      }
+    }
+    async initState() {
+      const configuration = await import_core.Configuration.find(
+        this.context.cwd,
+        this.context.plugins
+      );
+      const { project, workspace } = await import_core.Project.find(
+        configuration,
+        this.context.cwd
+      );
+      const cache = await import_core.Cache.find(configuration);
+      if (!workspace)
+        throw new import_cli.WorkspaceRequiredError(project.cwd, this.context.cwd);
+      await project.restoreInstallState();
+      const resolver = configuration.makeResolver();
+      return { configuration, workspace, cache, project, resolver };
+    }
+    async getAdvisories() {
+      const stdout = new import_stream.PassThrough();
+      const chunks = [];
+      stdout.on(`data`, (chunk) => {
+        chunks.push(chunk);
+      });
+      await this.cli.run([`npm`, `audit`, `-AR`, `--json`], { stdout });
+      stdout.end();
+      const result = JSON.parse(Buffer.concat(chunks).toString());
+      if (!isAuditResult(result)) {
+        throw new Error(`Unexpected yarn npm audit result`);
+      }
+      const advisories = [];
+      Object.entries(result.advisories).forEach(([_id, advisory]) => {
+        advisories.push(advisory);
+      });
+      return advisories;
     }
   };
   NpmAuditFixCommand.paths = [[`npm`, `audit`, `fix`]];
--- sources/index.ts
+++ sources/index.ts
@@ -1,20 +1,219 @@
-import { BaseCommand } from "@yarnpkg/cli"
-import { type Plugin } from "@yarnpkg/core"
-import { Option } from "clipanion"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { PassThrough } from "stream"
+
+import { BaseCommand, WorkspaceRequiredError } from "@yarnpkg/cli"
+import {
+  Cache,
+  Configuration,
+  Descriptor,
+  InstallMode,
+  MessageName,
+  Plugin,
+  Project,
+  semverUtils,
+  StreamReport,
+  structUtils,
+} from "@yarnpkg/core"
 import * as t from "typanion"
 
-const STRATEGIES = [`TODO`] as const
+const isAuditResult = t.isObject(
+  {
+    advisories: t.isDict(
+      t.isObject(
+        {
+          module_name: t.isString(),
+          vulnerable_versions: t.isString(),
+          patched_versions: t.isString(),
+        },
+        { extra: t.isDict(t.isUnknown()) },
+      ),
+    ),
+  },
+  { extra: t.isDict(t.isUnknown()) },
+)
+
+type AuditResult = t.InferType<typeof isAuditResult>
+type Advisory = AuditResult[`advisories`][string]
 
 class NpmAuditFixCommand extends BaseCommand {
   public static paths = [[`npm`, `audit`, `fix`]]
 
-  public strategy = Option.String(`--strategy`, `TODO`, {
-    description: `Set which strategy to attempt to fix advisories`,
-    validator: t.isEnum(STRATEGIES),
-  })
+  public async execute() {
+    const state = await this.initState()
+    const { configuration, project, cache } = state
 
-  public execute() {
-    return Promise.resolve(1)
+    const report = await StreamReport.start(
+      {
+        configuration,
+        stdout: this.context.stdout,
+      },
+      async (report) => {
+        const advisories = await report.startTimerPromise(`Audit step`, () =>
+          this.getAdvisories(),
+        )
+
+        for (const advisory of advisories) {
+          const fmtName = structUtils.prettyIdent(
+            configuration,
+            structUtils.parseIdent(advisory.module_name),
+          )
+          const fmtVulnRange = structUtils.prettyRange(
+            configuration,
+            advisory.vulnerable_versions,
+          )
+          const fmtPatchRange = structUtils.prettyRange(
+            configuration,
+            advisory.patched_versions,
+          )
+          await report.startTimerPromise(
+            `Advisory for ${fmtName} at ${fmtVulnRange}, patched at ${fmtPatchRange}`,
+            () => this.handleAdvisory(report, state, advisory),
+          )
+        }
+
+        await project.install({
+          report,
+          cache,
+          mode: InstallMode.UpdateLockfile,
+        })
+      },
+    )
+
+    return report.exitCode()
+  }
+
+  private async handleAdvisory(
+    report: StreamReport,
+    state: Awaited<ReturnType<typeof this.initState>>,
+    advisory: Advisory,
+  ) {
+    const { configuration, resolver, project, cache } = state
+
+    const ident = structUtils.parseIdent(advisory.module_name)
+
+    for (const descriptor of project.storedDescriptors.values()) {
+      if (!structUtils.areIdentsEqual(descriptor, ident)) {
+        continue
+      }
+
+      const locator = project.storedPackages.get(
+        project.storedResolutions.get(descriptor.descriptorHash),
+      )
+
+      if (
+        !semverUtils.satisfiesWithPrereleases(
+          locator.version,
+          advisory.vulnerable_versions,
+        )
+      ) {
+        continue
+      }
+
+      report.reportInfo(
+        MessageName.UNNAMED,
+        `Found vulnerable ${structUtils.prettyLocator(
+          configuration,
+          locator,
+        )} (via ${structUtils.prettyRange(configuration, descriptor.range)})`,
+      )
+
+      const candidates = await resolver.getCandidates(descriptor, new Map(), {
+        project,
+        report,
+        resolver,
+      })
+      const preferred = candidates[0]
+
+      if (!preferred) {
+        report.reportError(
+          MessageName.UNNAMED,
+          `No candidates found for ${structUtils.prettyDescriptor(
+            configuration,
+            descriptor,
+          )}`,
+        )
+        continue
+      }
+
+      const pkg = await resolver.resolve(preferred, {
+        project,
+        report,
+        resolver,
+      })
+      if (
+        !semverUtils.satisfiesWithPrereleases(
+          pkg.version,
+          advisory.patched_versions,
+        )
+      ) {
+        report.reportWarning(
+          MessageName.UNNAMED,
+          `No compatible patched version found for ${structUtils.prettyDescriptor(
+            configuration,
+            descriptor,
+          )}`,
+        )
+        continue
+      }
+
+      report.reportInfo(
+        MessageName.UNNAMED,
+        `Attempting to upgrade ${structUtils.prettyDescriptor(
+          configuration,
+          descriptor,
+        )} to ${structUtils.prettyLocator(configuration, pkg)}`,
+      )
+
+      project.storedResolutions.delete(descriptor.descriptorHash)
+      project.storedPackages.delete(locator.locatorHash)
+    }
+  }
+
+  private async initState() {
+    const configuration = await Configuration.find(
+      this.context.cwd,
+      this.context.plugins,
+    )
+
+    const { project, workspace } = await Project.find(
+      configuration,
+      this.context.cwd,
+    )
+
+    const cache = await Cache.find(configuration)
+
+    if (!workspace)
+      throw new WorkspaceRequiredError(project.cwd, this.context.cwd)
+
+    await project.restoreInstallState()
+
+    const resolver = configuration.makeResolver()
+
+    return { configuration, workspace, cache, project, resolver }
+  }
+
+  private async getAdvisories() {
+    const stdout = new PassThrough()
+    const chunks = []
+    stdout.on(`data`, (chunk) => {
+      chunks.push(chunk)
+    })
+
+    await this.cli.run([`npm`, `audit`, `-AR`, `--json`], { stdout })
+    stdout.end()
+
+    const result = JSON.parse(Buffer.concat(chunks).toString()) as unknown
+    if (!isAuditResult(result)) {
+      throw new Error(`Unexpected yarn npm audit result`)
+    }
+
+    const advisories: Advisory[] = []
+
+    Object.entries(result.advisories).forEach(([_id, advisory]) => {
+      advisories.push(advisory)
+    })
+
+    return advisories
   }
 }
 
